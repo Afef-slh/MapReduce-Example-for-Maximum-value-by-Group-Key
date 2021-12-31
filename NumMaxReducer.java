6840e070654d16b5ebb0b394e96b7614c7b3e4a5
Afef-slh <80926132+Afef-slh@users.noreply.github.com>
2021-12-31 23:11:05 +0100

    Update NumMaxReducer.java

--- NumMaxReducer.java
+++ NumMaxReducer.java
@@ -1,15 +1,13 @@
 package tn.isima.exercice;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 public class NumMaxReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-	ArrayList<Integer> List = new ArrayList<Integer>();
-
-
+@Override
 public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException{
     int max = 0;
    
