04156b2bf06b20933b5c9045063f4e974562ef8a
Afef-slh <80926132+Afef-slh@users.noreply.github.com>
2022-01-04 01:58:57 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
-import org.apache.hadoop.fs.FileSystem;
 
 import org.apache.hadoop.io.Text;
 
@@ -30,12 +29,6 @@ public class Main {
         Path outputFilePath = new Path(args[1]);
         FileInputFormat.addInputPath(job, inputFilePath);
         FileOutputFormat.setOutputPath(job, outputFilePath);
-        
-        
-        FileSystem fs = FileSystem.newInstance(conf);
-        if (fs.exists(outputFilePath)) {
-            fs.delete(outputFilePath, true);
-        }
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
